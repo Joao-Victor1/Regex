a12654a3be9a4c11dc9ef5802a37109332d53ba6
Joao Victor <s238672@gmail.com>
2023-03-20 15:10:28 -0300

    Regex em NodeJS

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 import chalk from "chalk";
 import fs from "fs";
 
+// const texto = "A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo.São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)";
+
+function extraiLinks(texto){
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm
+    // const linksExtraidos = texto.match(regex);
+    // const linksExtraidos = regex.exec(texto);
+    const arrayResultado = [];
+    let temp;
+    while((temp = regex.exec(texto)) != null){
+        arrayResultado.push({ [temp[1]] : [temp[2]] });
+    }
+    
+    return(arrayResultado);
+}
+
+// extraiLinks(texto);
+
 function trataErro(erro){
     // if(erro.code == "ENOENT"){
     //     console.log(chalk.red("Erro Enoent"))
     // }
-    throw new Error(chalk.red(erro.code, "Não há arquivo no caminho."));
+    throw new Error(chalk.red(erro.code, "Arquivo não ecnontrado."));
 }
 
 //Async Await com Try Catch:
 async function pegaArquivo(caminhoDoArquivo){
     try{
         const texto = await fs.promises.readFile(caminhoDoArquivo, 'utf-8');
-        console.log(chalk.blue(texto));
+        console.log(extraiLinks(texto));
     }catch(erro){
         trataErro(erro);
     }
@@ -37,6 +54,6 @@ async function pegaArquivo(caminhoDoArquivo){
 // }
 
 
-pegaArquivo('./Arquivos/texto.2md');
+pegaArquivo('./Arquivos/texto.md');
 
 
